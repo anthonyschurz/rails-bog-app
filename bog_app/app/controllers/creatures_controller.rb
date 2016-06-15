97e48d312e4a1cfdb7423c1fa6f4fd4e2b772d71
anthonyschurz <anthony.schurz@gmail.com>
2016-06-15 12:23:58 -0700

    first crack

---
+++ bog_app/app/controllers/creatures_controller.rb
@@ -0,0 +1,25 @@
+class CreaturesController < ApplicationController
+  # display all creatures
+   def index
+     @creatures = Creature.all
+     render :index
+   end
+
+   def new
+      render :new
+   end
+
+   def create
+      creature_params = params.require(:creature).permit(:name, :description)
+      creature = Creature.new(creature_params)
+      if creature.save
+        redirect_to creature_path
+   end
+
+   def show
+      creature_id = params[:id]
+      @creature = Creature.find_by_id(creature_id)
+      render :show
+   end
+
+end
